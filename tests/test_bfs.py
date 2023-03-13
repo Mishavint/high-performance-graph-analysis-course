a9966f6e41cd0e713a239d4ff0c1dbf38c718c8d
mishavint <mishavint@mail.ru>
2023-03-13 13:53:39 +0300

    Code style

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -6,9 +6,7 @@ from pygraphblas import Matrix
 class TestsForBfsLevels:
     def test_works_as_expected(self):
         graph = Matrix.from_lists(
-            [0, 1, 1, 2, 2, 5, 6, 3, 4, 7],
-            [1, 2, 3, 3, 5, 6, 7, 4, 7, 7],
-            True
+            [0, 1, 1, 2, 2, 5, 6, 3, 4, 7], [1, 2, 3, 3, 5, 6, 7, 4, 7, 7], True
         )
         assert bfs(graph, 1) == [-1, 0, 1, 1, 2, 2, 3, 3]
 
