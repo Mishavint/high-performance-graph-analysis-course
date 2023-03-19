fe791a9e29098fcbd8a733c7e5473abba3d9698b
mishavint <mishavint@mail.ru>
2023-03-19 13:00:50 +0300

    task2

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -1,5 +1,4 @@
-import pytest
-from project.bfs import bfs
+from project.bfs import bfs, ms_bfs
 from pygraphblas import Matrix
 
 
@@ -13,3 +12,15 @@ class TestsForBfsLevels:
         assert bfs(graph, 6) == [-1, -1, -1, -1, -1, -1, 0, 1]
 
         assert bfs(graph, 0) == [0, 1, 2, 2, 3, 3, 4, 4]
+
+
+class TestsForMSBFS:
+    def test_works_as_expected(self):
+        graph = Matrix.from_lists(
+            [0, 1, 1, 2, 2, 5, 6, 3, 4, 7], [1, 2, 3, 3, 5, 6, 7, 4, 7, 7], True
+        )
+        print(ms_bfs(graph, [0, 1]))
+        assert ms_bfs(graph, [0, 1]) == [
+            (0, [-1, 0, 1, 1, 3, 2, 5, 4]),
+            (1, [-2, -1, 1, 1, 3, 2, 5, 4]),
+        ]
